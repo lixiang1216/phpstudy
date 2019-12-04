a77d3d5d387027bc9e7633adf26e66b7ee334f1b
lixiang <945559567@qq.com>
2019-12-04 20:53:15 +0800

    Update web.php

--- laravel_study/routes/web.php
+++ laravel_study/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', 'SiteController@index');
+Route::get('/about', 'SiteController@about');
 Route::get('/book1', 'BookController@book');
 Route::namespace('Book')->group(function(){
     
